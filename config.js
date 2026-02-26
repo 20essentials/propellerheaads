65d772bca256a109d54fedb1de4370fe6f02bdab
DominicDev <s0l0estudi4r@gmail.com>
2026-02-26 13:20:16 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PROPELLERHEADS';
+export let nameSongs = [
+	"SPYBREAK!", /*1*/
+	"TAKE CALIFORNIA", /*2*/
+	"ON HER MAJESTY'S SECRET SERVICE", /*3*/
+	"360° (OH YEAH?)", /*4*/
+	"CRASH!", /*5*/
+	"VELVET PANTS", /*6*/
+	"GO FASTER", /*7*/
+	"ECHO & BOUNCE", /*8*/
+	"BETTER?", /*9*/
+	"OH YEAH!", /*10*/
+	"WINNING STYLE", /*11*/
+	"COMINAGETCHA", /*12*/
+	"DIVE!", /*13*/
+	"RON'S THEORY", /*14*/
+	"LETHAL CUT", /*15*/
+	"BIG DOG", /*16*/
+	"CLANG", /*17*/
+	"HISTORY REPEATING ", /*18*/
+	"PROPS VOTE OF GRATITUDE", /*19*/
+	"YOU WANT IT BACK", /*20*/
+]
